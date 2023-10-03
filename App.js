3322c5284b9a9a9ab9d4b30c23220ea18cb14445
KhanhNhat242 <86726746+KhanhNhat242@users.noreply.github.com>
2023-10-03 19:41:43 +0700

    2a and Tiki_Ok completed

---
+++ App.js
@@ -0,0 +1,20 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+import Cau2a from './components/2_a';
+import TikiPay from './components/tikiPay/TikiPayy'
+
+export default function App() {
+  return (
+    // <Cau2a />
+    <TikiPay />
+  );
+}
+
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     backgroundColor: '#fff',
+//     alignItems: 'center',
+//     justifyContent: 'center',
+//   },
+// });
